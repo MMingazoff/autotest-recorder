4b15582c38514beb74612620a51361aabc19f234
GitHub Action <action@github.com>
2024-10-01 16:01:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/mmingazoff/autotestrecorder/MyBundle.kt
+++ src/main/kotlin/com/github/mmingazoff/autotestrecorder/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mmingazoff.autotestrecorder
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mmingazoff/autotestrecorder/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/mmingazoff/autotestrecorder/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mmingazoff.autotestrecorder.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
